898bde5ca87b1f61589f2b0f9b4c42186d5fd8a1
Matyáš Skalický <matyaskalicky@gmail.com>
2017-10-13 15:30:34 +0200

    Added a MeshAgent test scene

---
+++ Assets/Scripts/AI/NavAgentTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentTest : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    public GameObject obstacle_prefab;
+    GameObject tempObstacle;
+
+    RaycastHit hit;
+
+    float place_rotation = 0f;
+
+	void Start () 
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () 
+    {
+        // Rotate the game object when placing
+        if (Input.GetKey(KeyCode.Q)) place_rotation -= 2f; 
+        if (Input.GetKey(KeyCode.E)) place_rotation += 2f;
+
+        // Press R to start placing an object
+        if (Input.GetKeyDown(KeyCode.R)) {
+            if (!tempObstacle) {
+                tempObstacle = Instantiate(obstacle_prefab, hit.point, Quaternion.Euler(0, place_rotation, 0));
+            }
+        }
+
+        // Press the left mouse button to release the GameObject currently holding
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            tempObstacle = null;
+		}
+
+        // Ray from the camera
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Raycast with the previously constructed ray
+        if (Physics.Raycast(ray, out hit, 100)) {
+            // If there is a temp obstacle in the scene rotate it
+            if (tempObstacle) {
+                tempObstacle.transform.position = hit.point;
+                tempObstacle.transform.rotation = Quaternion.Euler(0, place_rotation, 0);
+            }
+
+            // Set the target of the agent with a right click
+			if (Input.GetKeyDown(KeyCode.Mouse1)) {
+				agent.SetDestination(hit.point);
+			}
+
+        }
+             
+	}
+}
