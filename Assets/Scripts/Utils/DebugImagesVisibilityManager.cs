02d79a97980f2acef4d7667162367796e9eaed12
matyapav <matyapav@47cb3d70-d519-48dc-a79f-644d129a52f4>
2017-10-18 09:50:51 +0000

    Merge branch 'master' into svnsync
    git-svn-id: https://vcs.felk.cvut.cz/svn/courses/hry/team14/trunk@2 47cb3d70-d519-48dc-a79f-644d129a52f4

---
+++ Assets/Scripts/Utils/DebugImagesVisibilityManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugImagesVisibilityManager : MonoBehaviour {
+
+    public List<GameObject> toBeVisibleInDebug;
+    public bool isDebug = false;
+	
+	void Start()
+    {
+        if(isDebug) { 
+            foreach (GameObject obj in toBeVisibleInDebug)
+            {
+                foreach (Image img in obj.GetComponentsInChildren<Image>())
+                {
+                    img.enabled = true;
+                    Color color = img.color;
+                    color.a = 1f;
+                    img.color = color;
+                }
+            }
+        }
+    }
+}
