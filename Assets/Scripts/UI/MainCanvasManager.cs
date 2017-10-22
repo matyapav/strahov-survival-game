eb124bc983f89ef51ce764b9da1e1e1146db4ff6
Matyáš Skalický <matyaskalicky@gmail.com>
2017-10-23 00:51:10 +0200

    Added a basic simple and stupid UI

---
+++ Assets/Scripts/UI/MainCanvasManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCanvasManager : MonoBehaviour {
+
+    public GameObject gamePausedPanel;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gamePausedPanel != null && gamePausedPanel.activeSelf != true) {
+                gamePausedPanel.SetActive(true);
+                PauseGame();
+            }
+        }
+	}
+
+    public void PauseGame() {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1f;
+    }
+}
