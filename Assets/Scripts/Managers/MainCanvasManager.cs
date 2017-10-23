fa1743eadf11a1221936480ac7cd059770ba7fdf
Matyáš Skalický <matyaskalicky@gmail.com>
2017-10-23 14:34:51 +0200

    Switched to Editor Events

--- Assets/Scripts/Managers/MainCanvasManager.cs
+++ Assets/Scripts/Managers/MainCanvasManager.cs
@@ -8,18 +8,6 @@ public class MainCanvasManager : MonoBehaviourSingleton<MainCanvasManager> {
 
     public GameObject gamePausedPanel;
 
-    private void OnEnable()
-    {
-        MainEventManager.Instance.GamePauseEvent.AddListener(PauseGame);
-        MainEventManager.Instance.GameResumeEvent.AddListener(ResumeGame);
-    }
-
-    private void OnDisable()
-    {
-        MainEventManager.Instance.GamePauseEvent.RemoveListener(PauseGame);
-        MainEventManager.Instance.GameResumeEvent.RemoveListener(ResumeGame);
-    }
-
     // Update is called once per frame
     void Update () {
         if (Input.GetKeyDown(KeyCode.Escape)) {
