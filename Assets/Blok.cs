ad7c9421d0e4b0b6659ac121a7111f2ac5e47d4b
Matyáš Skalický <matyaskalicky@gmail.com>
2017-11-01 16:30:19 +0100

    Rebaked navmesh

---
+++ Assets/Blok.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blok : MonoBehaviour {
+
+
+    public float health = 100;
+    public string name = "BlokX";
+}
