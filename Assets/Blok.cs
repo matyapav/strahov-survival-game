0e4bf8e1cb7ef47ddb4e8000887be4c1c8ece50b
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-03 17:22:05 +0100

    minor scene changes

--- Assets/Blok.cs
+++ Assets/Blok.cs
@@ -19,7 +19,7 @@ public class Blok : MonoBehaviour {
     }
 
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
