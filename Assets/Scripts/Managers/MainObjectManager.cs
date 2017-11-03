6d788cfcaf45cd9a04562b98ce704717b1052d0c
matyapav <matyapav@47cb3d70-d519-48dc-a79f-644d129a52f4>
2017-11-03 16:15:50 +0000

    Rebaked navmesh
    git-svn-id: https://vcs.felk.cvut.cz/svn/courses/hry/team14/trunk@22 47cb3d70-d519-48dc-a79f-644d129a52f4

---
+++ Assets/Scripts/Managers/MainObjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainObjectManager : MonoBehaviourSingleton<MainObjectManager> {
+
+    public GameObject[] bloky;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
