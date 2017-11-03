1766a27f475efcc729b71ca2df3b0c31e3affa66
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-03 20:55:47 +0100

    obstacle placer - user now can't place objects if it is in collision with another one

--- Assets/Scripts/Utils/ObstaclePlacer.cs
+++ Assets/Scripts/Utils/ObstaclePlacer.cs
@@ -6,14 +6,31 @@ using UnityEngine.AI;
 public class ObstaclePlacer : MonoBehaviour {
 
     private GameObject tempObstacle;
+    private Color tempObstacleMaterialBackupColor;
     private RaycastHit hit;
     private float place_rotation = 0f;
+    private bool canPlace = true;
 
     public GameObject obstacle_prefab;
+
 
 	
 	// Update is called once per frame
 	void Update () {
+        if (tempObstacle) {
+            Debug.Log(tempObstacle.transform.localScale / 2);
+            if (Physics.OverlapBox(tempObstacle.transform.position, tempObstacle.transform.localScale / 2, tempObstacle.transform.rotation, LayerMask.GetMask("Obstacle")).Length > 0)
+            {
+                //hits another obstacle 
+                tempObstacle.GetComponent<Renderer>().material.color = Color.red;
+                canPlace = false;
+            } else
+            {
+                canPlace = true;
+                tempObstacle.GetComponent<Renderer>().material.color = tempObstacleMaterialBackupColor;
+            }
+         
+        }
         // Rotate the game object when placing
         if (Input.GetKey(KeyCode.Q)) place_rotation -= 2f;
         if (Input.GetKey(KeyCode.E)) place_rotation += 2f;
@@ -24,12 +41,14 @@ public class ObstaclePlacer : MonoBehaviour {
             if (!tempObstacle)
             {
                 tempObstacle = Instantiate(obstacle_prefab, hit.point, Quaternion.Euler(0, place_rotation, 0));
+                tempObstacleMaterialBackupColor = tempObstacle.GetComponent<Renderer>().material.color;
             }
         }
-
+        
         // Press the left mouse button to release the GameObject currently holding
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && tempObstacle != null && canPlace)
         {
+            tempObstacle.layer = LayerMask.NameToLayer("Obstacle");
             tempObstacle = null;
         }
 
