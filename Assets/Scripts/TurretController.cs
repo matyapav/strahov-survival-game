5438f51faa89d02d5233f982337aa34686838a2b
Matyáš Skalický <matyaskalicky@gmail.com>
2017-11-06 00:56:35 +0100

    Added a turret with a test scene

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class TurretController : MonoBehaviour {
+
+    private Transform TurretTop;
+    public Transform TargetTransform;
+    private ParticleSystem turretParticleSystem;
+
+    public float rotationSpeed = 3f;
+
+	void Start () {
+        turretParticleSystem = GetComponentInChildren<ParticleSystem>();
+        TurretTop = transform.GetChild(0);
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            turretParticleSystem.Play();    
+        }
+
+        if (TargetTransform != null && TurretTop != null) {
+            // Delta vector to the target
+             Vector3 delta = TargetTransform.position - TurretTop.transform.position;
+
+            // Get the current local rotation of the top
+            Vector3 curRotation = TurretTop.transform.localRotation.eulerAngles;
+
+            // Get the current rotation and calculate the desired
+            float desiredZRotation = Mathf.Atan2(delta.x, delta.z) * Mathf.Rad2Deg - 90;
+
+            // Lerp and assign
+            TurretTop.transform.localRotation = 
+                Quaternion.Euler(0, 0, Mathf.LerpAngle(curRotation.z, desiredZRotation, Time.deltaTime * rotationSpeed));
+        }
+	}
+}
