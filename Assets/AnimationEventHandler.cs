311edfa1f0b23c890dc5a95823bbbcc80c810288
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-08 00:41:05 +0100

    main menu

---
+++ Assets/AnimationEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEventHandler : MonoBehaviour {
+
+    [System.Serializable]
+    public class AnimationEvent : UnityEvent { };
+
+    public AnimationEvent onAnimationStart;
+    public AnimationEvent onAnimationEnd;
+
+    public void OnAnimationStart()
+    {
+        onAnimationStart.Invoke();
+    }
+
+    public void OnAnimationEnd()
+    {
+        onAnimationEnd.Invoke();
+    }
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
