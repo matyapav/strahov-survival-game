2bc9109e656f0413210e0e7c647807ada6993ce3
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-08 10:41:53 +0100

    transition between menu and main scene

--- Assets/Scripts/Managers/MainCanvasManager.cs
+++ Assets/Scripts/Managers/MainCanvasManager.cs
@@ -11,7 +11,7 @@ public class MainCanvasManager : MonoBehaviourSingleton<MainCanvasManager> {
     // Update is called once per frame
     void Update () {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (gamePausedPanel != null && gamePausedPanel.activeSelf != true) {
+            if (gamePausedPanel != null && gamePausedPanel.activeInHierarchy != true) {
                 MainEventManager.Instance.PauseGameEventInvoke();
             }
         }
