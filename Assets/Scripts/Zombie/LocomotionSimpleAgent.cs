cbd22876fd784f9dab0b5abc9f9a9f1163512a1f
stibipet <stibipet@fel.cvut.cz>
2017-11-14 23:55:38 +0100

    zombie animator extended, uses separate anim controller and model prefab - will be merged with the original later

--- Assets/Scripts/Zombie/LocomotionSimpleAgent.cs
+++ Assets/Scripts/Zombie/LocomotionSimpleAgent.cs
@@ -12,6 +12,8 @@ public class LocomotionSimpleAgent : MonoBehaviour
     private Vector2 smoothDeltaPosition = Vector2.zero;
     private Vector2 velocity = Vector2.zero;
 
+	private float distRemaining = 0.0f;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -19,10 +21,29 @@ public class LocomotionSimpleAgent : MonoBehaviour
 
         // Don’t update position automatically
         agent.updatePosition = false;
+		agent.acceleration = 6;
+		agent.speed = 3;
+		agent.angularSpeed = 900;
     }
 
     void Update()
     {
+		// DEBUG BLOCK
+		// Kills the zombie on K key press
+		if (Input.GetKeyDown (KeyCode.K)) {
+			anim.SetTrigger ("die");
+			agent.isStopped = true;
+		}
+		// Navigate to mouse cursor position on LMB click
+		/*if (Input.GetMouseButtonDown (1)) {
+				RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				agent.destination = hit.point;
+			}
+		}*/
+		// END DEBUG BLOCK
+
+		/*
 		Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
 
 		// Map 'worldDeltaPosition' to local space
@@ -42,6 +63,17 @@ public class LocomotionSimpleAgent : MonoBehaviour
 		
 		// Update animation parameters
 		anim.SetBool("isWalking", shouldMove);
+		*/
+
+		// TODO
+		// i když jsou nahardkóděný proměnný naprosto f pohodě, chtělo by to zpomalovat progresivně
+		if (agent.remainingDistance < 2.5f) {
+			agent.speed = 1;
+		} else {
+			agent.speed = 3;
+		}
+
+		anim.SetBool ("isWalking", agent.remainingDistance > 1.8f);
 
     }
 
