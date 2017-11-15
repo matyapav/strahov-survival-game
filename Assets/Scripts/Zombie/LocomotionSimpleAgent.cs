0743fb1e6743548608cdbaf8fe6efbc712b8ec70
stibipet <stibipet@fel.cvut.cz>
2017-11-15 14:52:21 +0100

    zombie animations extended + locomotion script completely reworked

--- Assets/Scripts/Zombie/LocomotionSimpleAgent.cs
+++ Assets/Scripts/Zombie/LocomotionSimpleAgent.cs
@@ -6,85 +6,75 @@ using System.Collections;
 [RequireComponent(typeof(Animator))]
 public class LocomotionSimpleAgent : MonoBehaviour
 {
+	public float movementSpeed = 2.7f;
+	public float lastStepLength = 0.4f;
+	public float stoppingDistance = 1.0f;
+	public float angleTurnLimit = 60;
+
     private Animator anim;
     private NavMeshAgent agent;
 
-    private Vector2 smoothDeltaPosition = Vector2.zero;
-    private Vector2 velocity = Vector2.zero;
-
-	private float distRemaining = 0.0f;
+	private Vector3 destination;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
 
-        // Don’t update position automatically
-        agent.updatePosition = false;
-		agent.acceleration = 6;
-		agent.speed = 3;
-		agent.angularSpeed = 900;
+		agent.speed = movementSpeed;
+		agent.angularSpeed = 900; //huge number helps with clumsy turning
+		agent.acceleration = 5;
+
+		// Removes ugly sliding on the last few steps
+		// but causes the agent to overshoot the target position sometimes
+		agent.autoBraking = false;
     }
 
     void Update()
     {
 		// DEBUG BLOCK
-		// Kills the zombie on K key press
+		// Kills the zombie on pressed K key
 		if (Input.GetKeyDown (KeyCode.K)) {
 			anim.SetTrigger ("die");
 			agent.isStopped = true;
 		}
 		// Navigate to mouse cursor position on LMB click
-		/*if (Input.GetMouseButtonDown (1)) {
+		// Double click needed for some reason
+		if (Input.GetMouseButtonDown (1)) {
 				RaycastHit hit;
 			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
-				agent.destination = hit.point;
+				agent.isStopped = false;
+				destination = hit.point;
 			}
-		}*/
-		// END DEBUG BLOCK
-
-		/*
-		Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
-
-		// Map 'worldDeltaPosition' to local space
-		float dx = Vector3.Dot(transform.right, worldDeltaPosition);
-		float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
-		Vector2 deltaPosition = new Vector2(dx, dy);
-		
-		// Low-pass filter the deltaMove
-		float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
-		smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
-		
-		// Update velocity if time advances
-		if (Time.deltaTime > 1e-5f)
-			velocity = smoothDeltaPosition / Time.deltaTime;
-		
-		bool shouldMove = velocity.magnitude > 0.5f && (agent.remainingDistance > agent.radius);
-		
-		// Update animation parameters
-		anim.SetBool("isWalking", shouldMove);
-		*/
-
-		// TODO
-		// i když jsou nahardkóděný proměnný naprosto f pohodě, chtělo by to zpomalovat progresivně
-		if (agent.remainingDistance < 2.5f) {
-			agent.speed = 1;
-		} else {
-			agent.speed = 3;
 		}
-
-		anim.SetBool ("isWalking", agent.remainingDistance > 1.8f);
-
+		// END DEBUG BLOCK
     }
 
-	void OnAnimatorMove() 
+	void FixedUpdate()
 	{
-		// Move the transform
-        if(agent == null)
-        {
-            agent = GetComponent<NavMeshAgent>();
-        }
-		transform.position = agent.nextPosition;
+		// Stop pathfinding on the last step
+		if (agent.remainingDistance < lastStepLength) {
+			agent.isStopped = true;
+		} 
+
+		// Limit movement speed when doing sharp turns
+		float turnAngle = Vector3.Angle (transform.forward, agent.destination);
+		if (turnAngle > angleTurnLimit) {
+			agent.speed = 0.3f;
+		} else {
+			agent.speed = movementSpeed;
+		}
+
+		// Call the correct animation
+		// TODO
+		anim.SetBool ("isWalking", agent.remainingDistance > stoppingDistance && turnAngle<=angleTurnLimit);
+		anim.SetBool ("isAttacking", agent.remainingDistance <= stoppingDistance); //TODO && nearObstacle && pathIncomplete
 	}
 
+	// Set a new destination point
+	void LateUpdate(){
+		if (!destination.Equals (agent.destination)) {
+			agent.SetDestination (destination);
+		}
+	}
  }
