b67aa25c8ae370e6b43ff8b8597fc429d5ef736e
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-25 12:54:11 +0100

    lamps at night

--- Assets/Scripts/Managers/DayNightController.cs
+++ Assets/Scripts/Managers/DayNightController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayNightController : MonoBehaviourSingleton<DayNightController> {
 
     public Animator lightAnimator;
+    public GameObject lamps;
+    public Text dayNightText;
     private DayNightPhase phase;
+    
+    private int dayCounter = 1;
 
     public DayNightPhase Phase
     {
@@ -18,6 +23,7 @@ public class DayNightController : MonoBehaviourSingleton<DayNightController> {
     public void Start()
     {
         phase = DayNightPhase.DAY;
+        dayNightText.text = "Day "+ dayCounter;
     }
 
     public void SwitchPhace()
@@ -27,11 +33,25 @@ public class DayNightController : MonoBehaviourSingleton<DayNightController> {
         {
             phase = DayNightPhase.NIGHT;
             MainUISoundManager.Instance.PlayNight();
+            dayNightText.text = "Night " + dayCounter;
+            SetLampsOn(true);
         }
         else if(phase == DayNightPhase.NIGHT)
         {
+            dayCounter++;
             phase = DayNightPhase.DAY;
             MainUISoundManager.Instance.PlayDay();
+            dayNightText.text = "Day " + dayCounter;
+            SetLampsOn(false);
+           
+        }
+    }
+
+    private void SetLampsOn(bool lampsOn)
+    {
+        foreach (Animator lampAnimator in lamps.transform.GetComponentsInChildren<Animator>(true))
+        {
+            lampAnimator.SetBool("on", lampsOn);
         }
     }
 
