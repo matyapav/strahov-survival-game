30f7757d8a97a40d20280a11cd58213fda0791f1
Matyáš Skalický <matyaskalicky@gmail.com>
2018-01-05 16:02:10 +0100

    Fixed the intro menu not loading up

---
+++ Assets/Scripts/Managers/MenuSceneController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Start with the camera animation on scene start
+	    Camera.main.GetComponent<Animator>().Play("CameraStart");	
+        Time.timeScale = 1;
+	}
+
+}
